ebae902623a017d4c614f1dd912f5ec3680485ee
nahyebae <nahye0812@naver.com>
2024-11-29 13:51:20 +0900

    Python Day1

---
+++ 1.py
@@ -0,0 +1,13 @@
+# print("Hello World")
+print("Hello", "Python", 1, 2, "asdfe", sep="\n") # \n줄바꿈
+print()
+print("1")
+print("안녕하세요", end = "")
+print("코딩온입니다.")
+
+ive = "I AM"
+print(ive)
+ive = "장원영"
+print(ive)
+Ive = "카리나"
+print(Ive, ive)
