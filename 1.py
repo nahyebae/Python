a0ea6cc32f9e505632a1937445be826740c9012f
nahyebae <nahye0812@naver.com>
2024-11-30 13:51:54 +0900

    Python Day2

--- 1.py
+++ 1.py
@@ -9,5 +9,37 @@ ive = "I AM"
 print(ive)
 ive = "장원영"
 print(ive)
-Ive = "카리나"
-print(Ive, ive)
+print(f"나는 {ive}입니다.")
+print("나는 ", ive, "입니다.", sep="")
+print("나는 " + ive +"입니다.")
+
+print(type(77))
+print(type(7.2))
+print(type("iv\"e"))
+print(type('iv\'e'))
+a = 77
+print(type(a))
+a = 7.2
+print(type(a))
+a = "ive"
+print(type(a))
+print("111\n111")
+print('111\'111')
+print("111'111")
+print('111"111')
+
+num = 10
+b_num = 0b1010
+h_num = 0xA
+
+print(num)
+print(b_num)
+print(h_num)
+
+print(bin(10))
+print(hex(10))
+
+print(ord('0'))
+print(ord('A'))
+print(chr(48))
+print(chr(65))
---
+++ 3.py
@@ -0,0 +1,32 @@
+# song = input("너의 최애 노래는?")
+# print(song)
+# print(type(song))
+
+# a = input("1+1=?")
+# # print(a)
+# print(int(a)*2) 
+# number = int(input())
+
+# a = 2
+# print(str(2)*10)
+# print(str(2)+"입니다")
+# print(2+"입니다") # 에러
+
+print("|\_/|")
+print("|q p|   /}")
+print('( 0 )\"\"\"\\')
+print('|"^"\`   |')
+print("||_/=\\\__|")
+
+# 실습3
+name = input("이름을 입력하세요. ")
+age = int(input("나이를 입력하세요. "))
+print("안녕하세요! "+ name + "님 " + "(" + str(age) + "세)\n")
+print("")
+
+
+name = input("이름을 입력하세요. ")
+birth = int(input("태어난 년도를 입력하세요. "))
+year = int(input("올해 년도를 입력하세요. "))
+print("올해는"+ str(year) +"년 ,"+ name +"님의 나이는"+ str(year-birth+1) + "세 입니다")
+
