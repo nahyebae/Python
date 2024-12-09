e76d509445c08d84c29f28009cb6994b2564ba6d
nahyebae <nahye0812@naver.com>
2024-12-09 13:49:06 +0900

    Day 9

---
+++ 7_class/24.py
@@ -0,0 +1,28 @@
+class Employee:
+    serial_num = 1000
+
+    def __init__(self, name):
+        Employee.serial_num += 1
+        self.id = Employee.serial_num
+        self.name = name
+    
+    def __str__(self):
+        return f"사번 : {self.id}, 이름 : {self.name}"
+    
+e1 = Employee("최사원")
+print(e1)
+
+e2 = Employee("양사원")
+print(e2)
+
+e3 = Employee("한사원")
+print(e3)
+
+employee = [
+    Employee('구름'),
+    Employee('별'),
+    Employee('행성'),
+    Employee('달')
+]
+for i in employee:
+    print(i)
